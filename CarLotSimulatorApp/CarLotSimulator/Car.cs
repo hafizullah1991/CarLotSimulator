ec939d485484e49ed21e456348cebefc77cecf2d
Hafiz Hamdard <ahmadshakhi2020@gmail.com>
2024-01-23 21:05:47 -0800

    exercise done

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CarLotSimulator
+{
+    internal class Car
+    {
+        public string Make { get; set; }
+        public int Year { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        public Car()
+        {
+            // Default constructor
+        }
+
+        public Car(string make, int year, string engineNoise, string honkNoise, bool isDriveable = true)
+        {
+            Make = make;
+            Year = year;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+            IsDriveable = isDriveable;
+        }
+
+        public void MakeEngineNoise()
+        {
+            Console.WriteLine($"The {Make} is making the engine noise: {EngineNoise} and is {IsDriveable}");
+        }
+
+        public void MakeHonkNoise()
+        {
+            Console.WriteLine($"The {Make} is making the honk noise: {HonkNoise} and is {IsDriveable}");
+        }
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 
 namespace CarLotSimulator
 {
@@ -13,10 +14,52 @@ namespace CarLotSimulator
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
             //The methods should take one string parameter: the respective noise property
 
+            // dot notation
+            Car firstCar = new Car();
+           firstCar.Make = "Bugatti Chiron";
+           firstCar.Year = 2024;
+           firstCar.EngineNoise = "Vroom Vroom";
+           firstCar.HonkNoise = "Loud Revving Horn";
+            firstCar.IsDriveable= true;
 
+            Console.WriteLine("======== initializer way==========");
+            Car firstCar2 = new Car() { Make = "Bugatti", Year = 2024, EngineNoise = "Vroom", HonkNoise = "Loud" };
+            Console.WriteLine($"{firstCar2.Make} {firstCar2.Year} {firstCar2.EngineNoise} {firstCar2.HonkNoise} {firstCar2.IsDriveable}");
+
+            Console.WriteLine("*************Custom Constructive****************");
+
+
+            Car customCar = new Car("Toyota", 2024, "Vroom Vroom", "loud", true);
+            Console.WriteLine($"{customCar.Make} {customCar.Year} {customCar.EngineNoise} {customCar.HonkNoise} {customCar.IsDriveable}");
+
+
+
+            Console.WriteLine("____________Example 2-----------------");
+            Car secondCar = new Car();
+            secondCar.Make = "Lamborghini Aventador";
+            secondCar.Year = 2023;
+            secondCar.EngineNoise = "Thunderous V12";
+            secondCar.HonkNoise = "Horn Blast";
+            secondCar.IsDriveable= true;
+
+
+
+            //Car thirdCar = new Car();
+            //thirdCar.Make = "Rolls-Royce Phanto";
+            //thirdCar.Year = 2023;
+            //thirdCar.EngineNoise = " V12 Elegance";
+            //thirdCar.HonkNoise = "Gentle Royce Melody";
+
+            //Call each of the methods for each car
+            firstCar.MakeEngineNoise();
+           
+            secondCar.MakeHonkNoise();
+
+            //thirdCar.MakeEngineNoise();
+            //thirdCar.MakeHonkNoise();
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
-            //Call each of the methods for each car
+       
 
             //*************BONUS*************//
 
